d0eb891760f131fc113ae7fa2344908e5ea12576
Harry Gordon <harrygordon1000@gmail.com>
2021-01-08 12:51:36 +0000

    Added a really simple example of the template config we need

---
+++ UmbracoPropertyFallbackExample/App_Plugins/FallbackTextstring/fallback-textstring.controller.js
@@ -0,0 +1,11 @@
+﻿var umbraco = angular.module('umbraco');
+
+umbraco.controller('FallbackTextstringController', ['$scope', function($scope) {
+
+    init();
+
+    $scope.placeholder = 'Placeholder text!';
+
+    function init() {
+    }
+}]);
