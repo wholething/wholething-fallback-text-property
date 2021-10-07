1ee7c4fc49896f7920886cf07aa3852b8331f1f4
Harry Gordon <harrygordon1000@gmail.com>
2021-10-07 13:23:55 +0100

    Added composer and controller

--- Wholething.FallbackTextProperty/Composers/FallbackTextComposer.cs
+++ Wholething.FallbackTextProperty/Composers/FallbackTextComposer.cs
@@ -1,27 +1,30 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Wholething.FallbackTextProperty.Services;
+﻿using Wholething.FallbackTextProperty.Services;
 using Wholething.FallbackTextProperty.Services.Impl;
 #if NET5_0_OR_GREATER
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 #else
 using Umbraco.Core.Composing;
 #endif
 
 namespace Wholething.FallbackTextProperty.Composers
 {
+#if NET5_0_OR_GREATER
     public class FallbackTextComposer : IComposer
     {
-#if NET5_0_OR_GREATER
         public void Compose(IUmbracoBuilder builder)
         {
             builder.Services.AddSingleton<IFallbackTextService, FallbackTextService>();
         }
+    }
 #else
+    public class FallbackTextComposer : IUserComposer
+    {
         public void Compose(Composition composition)
         {
-            composition.RegisterFor<IFallbackTextService, FallbackTextService>();
+            composition.Register(typeof(IFallbackTextService), typeof(FallbackTextService), Lifetime.Singleton);
         }
-#endif
     }
+#endif
 }
---
+++ Wholething.FallbackTextProperty/Controllers/FallbackTextController.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Wholething.FallbackTextProperty.Services;
+#if NET5_0_OR_GREATER
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.Attributes;
+#else
+using System.Web.Http;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+#endif
+
+namespace Wholething.FallbackTextProperty.Controllers
+{
+    [PluginController("FallbackText")]
+    public class TemplateDataController : UmbracoApiController
+    {
+        private readonly IFallbackTextService _fallbackTextService;
+
+        public TemplateDataController(IFallbackTextService fallbackTextService)
+        {
+            _fallbackTextService = fallbackTextService;
+        }
+
+#if NET5_0_OR_GREATER
+        [HttpGet]
+        public IActionResult Get([Required][Range(1, int.MaxValue)] int nodeId, [Required] string propertyAlias)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestResult();
+            }
+
+            return new OkObjectResult(_fallbackTextService.BuildDictionary(nodeId, propertyAlias));
+        }
+#else
+        [HttpGet]
+        public IHttpActionResult Get([Required][Range(1, int.MaxValue)] int nodeId, [Required] string propertyAlias)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_fallbackTextService.BuildDictionary(nodeId, propertyAlias));
+        }
+#endif
+    }
+}
