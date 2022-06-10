41ac05f46d81e4ddb3b16fc555c0f1692ef6cc77
Harry Gordon <harrygordon1000@gmail.com>
2022-06-10 14:14:34 +0100

    Switched to content keys (guid) and started on block support

--- Wholething.FallbackTextProperty/App_Plugins/FallbackTextstring/fallback-text-service.js
+++ Wholething.FallbackTextProperty/App_Plugins/FallbackTextstring/fallback-text-service.js
@@ -1,16 +1,53 @@
 ﻿var umbraco = angular.module('umbraco');
 
-umbraco.factory('fallbackTextService', ['$http', function ($http) {
+umbraco.factory('fallbackTextService', ['$http', 'eventsService', 'notificationsService', function ($http, eventsService, notificationsService) {
     var baseUrl = '/Umbraco/FallbackText';
 
+    var block = null;
+
+    var editorOpenUnsubscribe = eventsService.on(
+        'appState.editors.open',
+        function (event, args) {
+            console.log(args);
+            if (args.editor.view.includes('blockeditor')) {
+                if (block) {
+                    notificationsService.error(
+                        'Fallback editor',
+                        'Block editor opened from inside a block, fallback editor will not function correctly'
+                    );
+                }
+                block = args.editor.content;
+            }
+        }
+    );
+
+    var editorCloseUnsubscribe = eventsService.on(
+        'appState.editors.close',
+        function (event, args) {
+            if (args.editor.view.includes('blockeditor')) {
+                block = null;
+            }
+        }
+    );
+
+    //$scope.onDestroy = function () {
+    //    editorOpenUnsubscribe();
+    //}
+
     function getTemplateData(nodeId, propertyAlias, culture) {
+        console.log('getTemplateData', nodeId, propertyAlias);
         return $http.get(`${baseUrl}/TemplateData/Get?nodeId=${nodeId}&propertyAlias=${propertyAlias}&culture=${culture}`).then(function (data) {
             return data.data;
         });
     };
 
+    function getBlock() {
+        return block;
+    }
+
     var service = {
-        getTemplateData: getTemplateData
+        getTemplateData: getTemplateData,
+        getBlock: getBlock
     };
     return service;
 }]);
